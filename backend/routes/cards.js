47310422c310ca220d33840c19e4158146545a45
Nick <niickchen@users.noreply.github.com>
2018-09-18 19:30:49 -0700

    Update cards.js

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -6,6 +6,6 @@ router.get('/', Cards.startNewGame)
 router.post('/', Cards.startNewGame)
 router.get('/max', Cards.getMaxScore)
 router.post('/max', Cards.setMaxScore)
-router.post('/getColor', Cards.getColor)
+router.post('/getcolor', Cards.getColor)
 
 module.exports = router.routes()
